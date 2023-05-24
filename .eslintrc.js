cb393b9ef3b2a5629191f84fc3bbf1fdca37e5da
Hsin-Pao Huang <hsinpaohuang@gmail.com>
2023-05-24 22:19:29 +0800

    🧑‍💻 setup eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'vue/multi-word-component-names': 'off',
+    '@typescript-eslint/naming-convention': [
+      'warn',
+      {
+        selector: 'default',
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { varsIgnorePattern: '_', argsIgnorePattern: '_' },
+    ],
+  },
+  overrides: [
+    {
+      files: [
+        '**/pages/**/*.{js,ts,vue}',
+        '**/layouts/**/*.{js,ts,vue}',
+        '**/app.{js,ts,vue}',
+        '**/error.{js,ts,vue}',
+      ],
+    },
+  ],
+};
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  quoteProps: 'consistent',
+  arrowParens: 'avoid',
+};
