e2e0a62131d7e03db13b2f10b2baacdacd53e49a
hsinpaohuang <hsinpaohuang@gmail.com>
2023-09-17 00:00:51 +0100

    Add HTML report to CI mode

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Running in parallel seems to cause flaky tests */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? 'github' : 'html',
+  reporter: process.env.CI ? [['github'], ['html']] : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
